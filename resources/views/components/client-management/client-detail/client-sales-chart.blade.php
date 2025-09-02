24102bfa5800b165f8979162755c7a122bf06728
warmkatahira <t.katahira@warm.co.jp>
2025-09-02 14:30:19 +0900

    20250902_1

--- resources/views/components/client-management/client-detail/client-sales-chart.blade.php
+++ resources/views/components/client-management/client-detail/client-sales-chart.blade.php
@@ -1,4 +1,4 @@
-<div class="disable_scrollbar flex flex-grow overflow-scroll col-span-4 items-start">
+<div class="disable_scrollbar flex flex-grow overflow-scroll w-3/4 items-start">
     <div id="client_sales_chart_div" class="client_sales_chart overflow-x-auto overflow-y-auto border border-gray-600 w-full border-none flex flex-col gap-5">
     </div>
 </div>
