4c7fb2689ad2149f885574b02ff51a57a52e7466
warmkatahira <t.katahira@warm.co.jp>
2025-09-03 15:23:03 +0900

    20250903_1

--- resources/views/components/client-management/client-list/card.blade.php
+++ resources/views/components/client-management/client-list/card.blade.php
@@ -1,6 +1,12 @@
 <div class="w-full grid grid-cols-12 gap-4">
     @foreach($clients as $client)
         <div class="col-span-3 border border-gray-300 rounded-2xl shadow-md p-4 flex flex-col items-center {{ $client->is_active ? 'bg-white' : 'bg-gray-300' }} transition duration-300 ease-in-out transform hover:-translate-y-2 hover:shadow-xl">
+            <div class="flex items-center gap-2 mb-2 text-xs text-gray-500">
+                <img class="profile_image_normal flex-shrink-0 tippy_user_full_name image_fade_in_modal_open" src="{{ asset('storage/profile_images/'.$client->user->profile_image_file_name) }}" data-user-full-name="{{ $client->user->full_name }}">
+                <span class="whitespace-nowrap text-xs">
+                    {{ CarbonImmutable::parse($client->updated_at)->isoFormat('Y年MM月DD日(ddd) HH:mm:ss').' ('.CarbonImmutable::parse($client->updated_at)->diffForHumans().')' }}
+                </dpan>
+            </div>
             <div class="w-full h-40 flex justify-center items-center {{ $client->is_active ? 'bg-gray-100' : 'bg-gray-300' }} rounded-xl overflow-hidden">
                 <img src="{{ asset('storage/client_images/'.$client->client_image_file_name) }}" class="w-40 h-28 object-contain image_fade_in_modal_open">
             </div>
