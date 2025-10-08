55f000be3feb298f1cb20a22b4cadc2e43992caf
warmkatahira <t.katahira@warm.co.jp>
2025-10-08 15:40:26 +0900

    20251008_1

--- app/Http/Requests/ClientManagement/ClientUpdate/BasicInfoUpdateRequest.php
+++ app/Http/Requests/ClientManagement/ClientUpdate/BasicInfoUpdateRequest.php
@@ -26,7 +26,7 @@ class BasicInfoUpdateRequest extends BaseRequest
             'client_status_id'          => 'required|exists:client_statuses,client_status_id',
             'client_code'               => 'required|integer|min:1|unique:clients,client_code,'.$this->client_id.',client_id',
             'client_name'               => 'required|string|max:100|unique:clients,client_name,'.$this->client_id.',client_id',
-            'client_postal_code'        => 'nullable|string|max:8',
+            'client_postal_code'        => 'nullable|string|min:8|max:8',
             'prefecture_id'             => 'nullable|exists:prefectures,prefecture_id',
             'client_address'            => 'nullable|string|max:255',
             'client_tel'                => 'nullable|string|max:13',
